4bee46200526be0e946f016a05802e07f27e9e73
oreoluwa212 <105554286+oreoluwa212@users.noreply.github.com>
2023-09-21 11:18:02 +0100

    Postcode App

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,7 @@
 import Logo from '../assets/img/home.png'
 import Vector from '../assets/img/Vector.png'
+import Asset8 from '../assets/img/asset 8 1.png'
+import Asset7 from '../assets/img/asset 7.png'
+import Asset9 from '../assets/img/asset 9 1.png'
 
-export { Logo, Vector }
+export { Logo, Vector, Asset7, Asset8, Asset9 }
--- src/components/BookingSection.jsx
+++ src/components/BookingSection.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Asset7, Asset8, Asset9 } from '../assets';
 
 const BookingSection = () => {
   return (
     <div className="booking-section-container">
       <div className="booking-section">
         <div className="booking-group">
-          <h1>BookingSection</h1>
+          <div className="booking-upper">
+            <h2>Booking.com</h2>
+            <div className="booking">
+              <div className="bg-white"></div>
+            </div>
+          </div>
+          <div className="booking-grid">
+            <div className="grid1">
+              <img src={Asset7} alt="" />
+            </div>
+            <div className="grid1">
+              <img src={Asset8} alt="" />
+            </div>
+            <div className="grid1">
+              <img src={Asset9} alt="" />
+            </div>
+            <div className="grid1"></div>
+          </div>
         </div>
       </div>
     </div>
