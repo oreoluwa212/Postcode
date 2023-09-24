5b543adb373aa1b18e10e24faeefe0b53bce7aa2
oreoluwa212 <105554286+oreoluwa212@users.noreply.github.com>
2023-09-24 08:54:32 +0100

    Postcoe

--- src/components/PopularPostcode.jsx
+++ src/components/PopularPostcode.jsx
@@ -2,8 +2,153 @@ import React from 'react'
 
 const PopularPostcode = () => {
   return (
-    <div className='popular-container'>PopularPostcode</div>
-  )
+    <div className="popular-container">
+      <div className="popular-postcodes">
+        <p>Postcodes</p>
+        <h1>Popular Postcodes</h1>
+        <h3>
+          Easily find the popular postcodes by clicking on any of the displayed
+          postcodes.
+        </h3>
+        <div className="first">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="second">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="first">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="second">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="first">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="second">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="first">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="second">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="first">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="second">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="first">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="second">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="first">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="second">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="first">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="second">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+        <div className="first">
+          <h4>SA99 1DU</h4>
+          <h4>W1A 1AA</h4>
+          <h4>SW1A 1AA</h4>
+          <h4>WC2N 5DU</h4>
+          <h4>LE87 2BB</h4>
+          <h4>SL6 2QQ</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PopularPostcode
