682eccbec7f08578c31b5757de5018f6c58bea0f
oreoluwa212 <105554286+oreoluwa212@users.noreply.github.com>
2023-09-26 09:58:24 +0100

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,57 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer-inner">
-         <h1>Footer goes here</h1>
+        <div className="footer-grid">
+          <div className="first">
+            <ul>
+              <li>
+                <a href="">Search</a>
+              </li>
+              <li>
+                <a href="">Postcode by Country</a>
+              </li>
+              <li>
+                <a href="">Postcode by Introduction Date</a>
+              </li>
+              <li>
+                <a href="">Postcode Near Me</a>
+              </li>
+              <li>
+                <a href="">Postcode Lottery</a>
+              </li>
+            </ul>
+          </div>
+          <div className="second">
+            <ul>
+              <li>
+                <a href="">Postcode Map by User Type</a>
+              </li>
+              <li>
+                <a href="">Postcode Map by User Category</a>
+              </li>
+              <li>
+                <a href="">Postcode Map by Council Tax Band</a>
+              </li>
+            </ul>
+          </div>
+          <div className="second">
+            <ul>
+              <li>
+                <a href="">About this Website</a>
+              </li>
+              <li>
+                <a href="">Disclaimer and Limitations</a>
+              </li>
+              <li>
+                <a href="">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="copyright">
+        <h5>Check my Postcode</h5>
+        <p>CheckMyPostcode.UK is a Good Stuff Website</p>
       </div>
     </div>
   );
